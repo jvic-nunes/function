4c0ca2cae9b3c29846ac96e2c0a11326fc61b810
jv-nunes <jvnlino@gmail.com>
2024-01-25 16:41:52 -0300

    first commit

---
+++ function.js
@@ -0,0 +1,14 @@
+/* function nome(nome) {
+    console.log(nome);
+}
+nome("joão victor");
+nome("Eduardo"); */
+
+function soma(num1, num2) {
+    //console.log(num1 + num2)
+    let resultado = num1 + num2
+    return resultado
+}
+console.log(soma(5, 7))
+
+
